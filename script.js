a4f7df25a0aa1ded5de95029ee3df834e0bc073e
Michael <mbahl1670@gmail.com>
2021-10-22 22:40:26 -0500

    Added code to ask for each of the criteria requirements

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 // Assignment code here
 var generatePassword = function() {  
   console.log("Generate a password")
-  var newPassword = "Password";
+  
+  // declare the newPassword as an object with each of the password criteria as a key of the object
+  var newPassword = {
+    length: 8,
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    specialChar: true
+  };
+  
+  console.log(newPassword);
   // Ask for pasword criteria with a series of promps
-
   // Ask for length of the password, accept values between 8 - 128 characters long, only allow a valid response
+  console.log("How many characters do you want your password to be?  Please specific a number between 8 and 128.");
+  newPassword.length = window.prompt("How many characters do you want your password to be?  Please specific a number between 8 and 128.");
+
 
   /// Ask if uppercase letters allowed, only allow a valid response
+  console.log("Do you want to include UPPERCASE letters in your passwsord?");
+  newPassword.uppercase = window.confirm("Do you want to include UPPERCASE letters in your passwsord?");
+
 
   // Ask if lowercase letters allowed, only allow a valid response
+  console.log("Do you want to include 'lowercase' letters in your passwsord?");
+  newPassword.lowercase = window.confirm("Do you want to include 'lowercase' letters in your passwsord?");
+
 
   // Ask if numbers allowed, only allow a valid resposne
-
+  console.log("Do you want to include numbers in your passwsord?");
+  newPassword.numbers = window.confirm("Do you want to include numbers in your passwsord?");
+  
   // Ask if special characters allowed, only allow a valid resposne
+  console.log("Do you want to include special characters in your passwsord?");
+  newPassword.specialChar = window.confirm("Do you want to include special characters in your passwsord?");
 
   // Verify that at least one character type has been selected
+  if (newPassword.lowercase) {
+    console.log("lowercase is true");
+  }
+  else {
+    console.log("This isn't working");
+  }
 
   //Password is generated
 
   
   // have the function return the new password
-  return newPassword;
+  return newPassword.length;
 };
 
 
